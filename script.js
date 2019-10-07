8eecbcee9fdf0f047519b55c99f5b40bc3f709f9
Denis <example@gmail.com>
2019-10-07 15:39:32 +0300

    123

---
+++ script.js
@@ -0,0 +1,20 @@
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", ''),
+     
+    appData.expenses[a] = b;
+}
+
+alert(appData.budget / 30);
